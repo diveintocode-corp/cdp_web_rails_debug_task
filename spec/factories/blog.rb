5afc9923f5b620240bb6d2fc84b400c7da916ce1
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2020-11-19 14:47:28 +0900

    Update blog.rb

--- spec/factories/blog.rb
+++ spec/factories/blog.rb
@@ -4,9 +4,4 @@ FactoryBot.define do
     title { 'Factoryで作ったデフォルトのタイトル１' }
     content { 'Factoryで作ったデフォルトのコンテンツ１' }
   end
-
-  # factory :second_task, class: Task do
-  #   title { 'Factoryで作ったデフォルトのタイトル２' }
-  #   content { 'Factoryで作ったデフォルトのコンテント２' }
-  # end
 end
